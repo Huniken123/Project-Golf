9c42bb931fe2d6fdb722ea1b5da15ad091f5c36a
Jaden S <jbroxnari@protonmail.com>
2021-09-24 11:17:43 -0400

    Camera moves smoother
    Working on drag shot next

--- Golf Gaem/Assets/Scripts/Player Control/ControlPoint.cs
+++ Golf Gaem/Assets/Scripts/Player Control/ControlPoint.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class ControlPoint : MonoBehaviour
 {
-    // Remember OnMouseDrag
-
     float xRot, yRot = 0f;
 
     public Rigidbody ball;
 
     public float rotationSpeed = 2f;
     public float shootPower = 30f;
+    public float lineLength = 4f;
+    //dragback strength edit the latter two vars
 
     public LineRenderer line;
 
@@ -30,12 +30,11 @@ public class ControlPoint : MonoBehaviour
         if (yRot > 35f) yRot = 35f;
         transform.rotation = Quaternion.Euler(yRot, xRot, 0f);
 
-
         if (Input.GetMouseButton(0))
         {
             line.gameObject.SetActive(true);
             line.SetPosition(0, transform.position);
-            line.SetPosition(1, transform.position + transform.forward * 4f);
+            line.SetPosition(1, transform.position + transform.forward * lineLength);
         }
         if (Input.GetMouseButtonUp(0))
         {
