fdf5cc4219d4289a9008731fd0617a97ef4e28cd
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2021-10-03 01:34:20 -0400

    Updated Controls to include a drag-release functionality

--- Golf Gaem/Assets/Scripts/Player Control/ControlPoint.cs
+++ Golf Gaem/Assets/Scripts/Player Control/ControlPoint.cs
@@ -9,15 +9,25 @@ public class ControlPoint : MonoBehaviour
     public Rigidbody ball;
 
     public float rotationSpeed = 2f;
-    public float shootPower = 30f;
+    [SerializeField]float shootPower;
     public float lineLength = 4f;
     //dragback strength edit the latter two vars
 
     public LineRenderer line;
 
+    float dragStartPos;     // Tobey - The Y position of where the player pressed the screen
+    float dragReleasePos;   // Tobey - The Y position of where the player released the screen
+
+    bool isShooting;        // Tobey -  Checks if the player is shooting
+    bool isShot;            // Tobey - Checks if the ball has been shot
+
+
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;     // Tobey - Confines the cursor into the window
+        isShooting = false;
+        isShot = false;
     }
 
     private void Update()
@@ -30,16 +40,78 @@ public class ControlPoint : MonoBehaviour
         if (yRot > 35f) yRot = 35f;
         transform.rotation = Quaternion.Euler(yRot, xRot, 0f);
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0) && !isShooting)
         {
-            line.gameObject.SetActive(true);
-            line.SetPosition(0, transform.position);
-            line.SetPosition(1, transform.position + transform.forward * lineLength);
+            DragStart();
+
+            Debug.Log("ball pressed: " + Input.mousePosition);
         }
+
         if (Input.GetMouseButtonUp(0))
         {
-            ball.velocity = transform.forward * shootPower;
-            line.gameObject.SetActive(false);
+            isShot = true;
+        }
+
+        //LineRendererUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!Input.GetMouseButtonDown(0) && isShooting)
+        {
+            //ball.velocity = transform.forward * shootPower;
+            //line.gameObject.SetActive(false);
+            DragRelease();
+            
         }
     }
+
+    void DragStart()
+    {
+        dragStartPos = Input.mousePosition.y;
+        isShooting = true;
+    }
+
+    void DragRelease()
+    {
+        if (isShot)
+        {
+            isShooting = false;
+            ball.velocity = new Vector3(0,0,0);
+        }
+
+
+        if(isShooting == false)
+        {
+            dragReleasePos = Input.mousePosition.y;
+            shootPower = (dragStartPos - dragReleasePos) / 10;
+            ball.velocity = (transform.forward * (shootPower));
+            isShot = false;
+            Debug.Log("ball released: " + Input.mousePosition);
+            Debug.Log(shootPower);
+
+            dragStartPos = 0;
+            dragReleasePos = 0;
+        }
+    }
+
+    /*
+    void LineRendererUpdate()
+    {
+        Vector3 forceInit = new Vector3(0, Input.mousePosition.y - dragStartPos / 10, 0);
+        Vector3 forceV = (new Vector3(-forceInit.y, forceInit.y, 0) * shootPower);
+
+        if (Input.GetMouseButton(0))
+        {
+            LineTrajectory.Instance.UpdateTrajectory(forceVector: forceV, rigidbody: ball, startingPoint: ball.transform.position);
+        }
+
+        
+        if (Input.mousePosition.y > dragStartPos + 3.5f)
+        {
+            LineTrajectory.Instance.HideLine();
+        }
+        
+    }
+    */
 }
