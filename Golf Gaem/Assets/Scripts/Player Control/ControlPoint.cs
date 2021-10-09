a1bfc970defc0571f34886df46370fd5714b324f
Jaden S <jbroxnari@protonmail.com>
2021-10-08 23:33:16 -0400

    Golf movement refinements
    Camera locks when shooting, attempt at making shot preview work with new shot system but not there yet
    
    Also tested Havok then got rid of it

--- Golf Gaem/Assets/Scripts/Player Control/ControlPoint.cs
+++ Golf Gaem/Assets/Scripts/Player Control/ControlPoint.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class ControlPoint : MonoBehaviour
 {
     float xRot, yRot = 0f;
+    public float rotationSpeed = 2f;
 
     public Rigidbody ball;
 
-    public float rotationSpeed = 2f;
     float shootPower;   //  Tobey - This now holds the equation for (dragStartPos - dragReleasePos / shotDivider)
-    public float lineLength = 4f;
-    [SerializeField] float shotdivider; //  Tobey - This is the float that is used to divide the shootPower equation
+    [SerializeField] float shotdivider = 7; //  Tobey - This is the float that is used to divide the shootPower equation
+    Vector3 ballLastShot;
+    [SerializeField] float killboxY = -10; //change this depending on level
 
     public LineRenderer line;
+    public float lineLength = 4f;
 
     float dragStartPos;     // Tobey - The Y position of where the player pressed the screen
     float dragReleasePos;   // Tobey - The Y position of where the player released the screen
@@ -32,53 +34,60 @@ public class ControlPoint : MonoBehaviour
     private void Update()
     {
         transform.position = ball.position;
-
-
+        #region Camera Movement
         xRot += Input.GetAxis("Mouse X") * rotationSpeed;
-        yRot += Input.GetAxis("Mouse Y") * rotationSpeed;
+        if (!isShooting) yRot += Input.GetAxis("Mouse Y") * rotationSpeed;
+        // lock camera y axis while shooting
         if (yRot < -35f) yRot = -35f;
         if (yRot > 35f) yRot = 35f;
-        transform.rotation = Quaternion.Euler(yRot, xRot, 0f);
+        transform.rotation = Quaternion.Euler(yRot, xRot, 0f); // cam control
+        #endregion
 
         ball.transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);     //  Tobey - Sets the Y axis of the ball to the Y axis of the Control Point
 
-        if (Input.GetMouseButtonDown(0) && !isShooting)
+        if (Input.GetMouseButtonDown(0) && !isShooting) DragStart();
+        if (Input.GetMouseButtonDown(0))
         {
-            DragStart();
-
-            //Debug.Log("ball pressed: " + Input.mousePosition);
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, transform.position + transform.forward * lineLength);
         }
 
         //  Tobey - This is a workaround for releasing the button in Update and Fixed Update
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0)) isShot = true; line.gameObject.SetActive(false);
+
+        if (ball.position.y <= killboxY)
         {
-            isShot = true;
+            ball.position = ballLastShot;
+            Debug.LogWarning("Ball out of bounds, respawning at " + ballLastShot);
+            ball.velocity = new Vector3(0,0,0);
         }
 
         //  Tobey - This is code related to the Line Trajectory UI. I'd recommend commenting it out as it doesn't fully work at the moment.
-        #region Line Trajectory Code
+        /*#region Line Trajectory Code
         Vector3 forceV = ball.velocity * shootPower;
 
         if(Input.GetMouseButton(0))
         {
             LineTrajectory.Instance.UpdateTrajectory(forceVector: forceV, ball, startingPoint: transform.position);
         }
-        #endregion
+        #endregion*/
     }
 
     private void FixedUpdate()
     {
         if (!Input.GetMouseButtonDown(0) && isShooting)
         {
-            DragRelease();  
+            DragRelease();
+            line.gameObject.SetActive(true);
         }
     }
 
     void DragStart()    //  Tobey - Sets up the properties for dragging the ball
     {
         dragStartPos = Input.mousePosition.y;
+        ballLastShot = ball.transform.position;
+        Debug.Log("Ball spawn stored. Location: " + ballLastShot);
         isShooting = true;
-        
     }
 
     void DragRelease()  //  Tobey - Sets up the properties for releasing the ball
@@ -87,25 +96,29 @@ public class ControlPoint : MonoBehaviour
         {
             isShooting = false;
             ball.velocity = new Vector3(0,0,0); // Tobey - Fixes a glitch and also stops any ball momentum with a single click. Potentially good for precise platforming if worked on more.
+            // maybe keep this in for space level
         }
-
 
         if(isShooting == false)
         {
             dragReleasePos = Input.mousePosition.y;
             shootPower = (dragStartPos - dragReleasePos) / shotdivider;
 
-            ball.velocity = transform.forward * shootPower;
+            if (shootPower >= 1) ball.velocity = transform.forward * shootPower;
             isShot = false;
 
+            Debug.Log("Ball released: " + Input.mousePosition);
 
-            Debug.Log("ball released: " + Input.mousePosition);
-
-            Debug.Log(shootPower);
+            if (shootPower >= 1) Debug.Log(shootPower);
+            else Debug.Log("Shot wasn't strong enough");
 
             //  Tobey - Prevents numbers from being stored after the equation
-            dragStartPos = 0;   
-            dragReleasePos = 0;
+            dragStartPos = 0; dragReleasePos = 0;
         }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("OutOfBounds")) ball.transform.position = ballLastShot;
     }
 }
