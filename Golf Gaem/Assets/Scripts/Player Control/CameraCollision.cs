df59e29b6745f171ed9739e45d3d6628809c491a
Jaden S <jbroxnari@protonmail.com>
2021-11-08 22:49:30 -0500

    SMART THIRD PERSON CAMERA (but it needs tweaking)

---
+++ Golf Gaem/Assets/Scripts/Player Control/CameraCollision.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    // adapted from this tutorial: https://sharpcoderblog.com/blog/third-person-camera-in-unity-3d
+
+    public Transform referenceTransform;
+    public float collisionOffset = 0.3f; //To prevent Camera from clipping through Objects
+    public float cameraSpeed = 15f; //How fast the Camera should snap into position if there are no obstacles
+
+    Vector3 defaultPos;
+    Vector3 directionNormalized;
+    Transform parentTransform;
+    float defaultDistance;
+
+    void Start()
+    {
+        defaultPos = transform.localPosition;
+        directionNormalized = defaultPos.normalized;
+        parentTransform = transform.parent;
+        defaultDistance = Vector3.Distance(defaultPos, Vector3.zero);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 currentPos = defaultPos;
+        RaycastHit hit;
+        Vector3 dirTmp = parentTransform.TransformPoint(defaultPos) - referenceTransform.position;
+        if (Physics.SphereCast(referenceTransform.position, collisionOffset, dirTmp, out hit, defaultDistance))
+        {
+            currentPos = (directionNormalized * (hit.distance - collisionOffset));
+
+            transform.localPosition = currentPos;
+        }
+        else
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, currentPos, Time.deltaTime * cameraSpeed);
+        }
+    }
+}
