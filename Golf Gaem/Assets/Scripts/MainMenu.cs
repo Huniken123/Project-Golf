b282e01d1832f9dae6db2f30ff3a47d303c945d9
Jaden S <jbroxnari@protonmail.com>
2021-11-08 21:09:58 -0500

    Added TMPro and a main menu
    woooo

---
+++ Golf Gaem/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MMenu;
+    public GameObject OMenu;
+    public GameObject HTP;
+
+    public void PrePlay()
+    {
+        MMenu.SetActive(false);
+        HTP.SetActive(true);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+    
+    public void HTPBack()
+    {
+        MMenu.SetActive(true);
+        HTP.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        MMenu.SetActive(false);
+        OMenu.SetActive(true);
+    }
+    
+    public void OptionsBack()
+    {
+        MMenu.SetActive(true);
+        OMenu.SetActive(false);
+    }
+}
