54a0e73dbcc0a10a1caa1d7cdf5b0c2ed6f2cc0a
AHogan3 <ashogan194@gmail.com>
2021-11-17 11:10:10 -0500

    Collectables now count total in level

--- Golf Gaem/Assets/Aaron's folder/Collect.cs
+++ Golf Gaem/Assets/Aaron's folder/Collect.cs
@@ -1,15 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collect : MonoBehaviour
 {
+    public GameObject scoreText;
+    public static int theScore;
+    public List<GameObject> collectList = new List<GameObject>();
+    public int totalCollect;
+
     public AudioSource collectSound;
+
+    void Start()
+    {
+        foreach (GameObject collectableObjs in GameObject.FindGameObjectsWithTag("Collectable"))
+        {
+            collectList.Add(collectableObjs);
+        }
+
+        foreach (var x in collectList)
+        {
+            totalCollect += 1;
+        }
+    }
+
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = "Score: " + theScore + "/" + totalCollect;
+    }
+
 
     void OnTriggerEnter(Collider other)
     {
         collectSound.Play();
-        ScoringSystem.theScore += 50;
+        theScore += 1;
         Destroy(gameObject);
     }
 }
