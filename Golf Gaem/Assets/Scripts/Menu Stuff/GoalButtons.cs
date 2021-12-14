30c55b1e94892152c03cdc34b42f02b02197626b
Jaden S <jbroxnari@protonmail.com>
2021-12-14 14:51:14 -0500

    Added the grading system at the end

--- Golf Gaem/Assets/Scripts/Menu Stuff/GoalButtons.cs
+++ Golf Gaem/Assets/Scripts/Menu Stuff/GoalButtons.cs
@@ -8,7 +8,9 @@ public class GoalButtons : MonoBehaviour
 {
     public Text lobbyText, m1Text, m2Text;
     public Text lobbyColText, m1ColText, m2ColText;
+    public Text gradeText;
 
+    int goodBoyPoints = 0;
     int lobbyPar = 3;
     int m1Par = 20;
     int m2Par = 8;
@@ -18,18 +20,43 @@ public class GoalButtons : MonoBehaviour
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
         lobbyText.text = "Lobby Score: " + ParManager.lobbyScore;
-        if (ParManager.lobbyScore <= lobbyPar) lobbyText.color = Color.yellow;
+        if (ParManager.lobbyScore <= lobbyPar) { lobbyText.color = Color.yellow; goodBoyPoints++; }
         m1Text.text = "Maintenance 1 Score: " + ParManager.m1Score;
-        if (ParManager.m1Score <= m1Par) m1Text.color = Color.yellow;
+        if (ParManager.m1Score <= m1Par) { m1Text.color = Color.yellow; goodBoyPoints++; }
         m2Text.text = "Maintenance 2 Score: " + ParManager.m2Score;
-        if (ParManager.m2Score <= m2Par) m2Text.color = Color.yellow;
+        if (ParManager.m2Score <= m2Par) { m2Text.color = Color.yellow; goodBoyPoints++; }
 
         lobbyColText.text = "Collectibles: " + ParManager.lobbyCollected + "/" + ParManager.lobbyColTotal;
-        if (ParManager.lobbyCollected == ParManager.lobbyColTotal) lobbyColText.color = Color.yellow;
+        if (ParManager.lobbyCollected == ParManager.lobbyColTotal) { lobbyColText.color = Color.yellow; goodBoyPoints++; }
         m1ColText.text = "Collectibles: " + ParManager.m1Collected + "/" + ParManager.m1ColTotal;
-        if (ParManager.m1Collected == ParManager.m1ColTotal) m1ColText.color = Color.yellow;
+        if (ParManager.m1Collected == ParManager.m1ColTotal) { m1ColText.color = Color.yellow; goodBoyPoints++; }
         m2ColText.text = "Collectibles: " + ParManager.m2Collected + "/" + ParManager.m2ColTotal;
-        if (ParManager.m2Collected == ParManager.m2ColTotal) m2ColText.color = Color.yellow;
+        if (ParManager.m2Collected == ParManager.m2ColTotal) { m2ColText.color = Color.yellow; goodBoyPoints++; }
+
+        switch (goodBoyPoints)
+        {
+            case 0:
+            case 1:
+            case 2:
+                gradeText.text = "Try again for a better score!";
+                break;
+            case 3:
+            case 4:
+                gradeText.text = "Great job!";
+                break;
+            case 5:
+                gradeText.text = "EXCELLENT!";
+                gradeText.color = Color.yellow;
+                break;
+            case 6:
+                gradeText.text = "How's this even possible???";
+                gradeText.color = Color.yellow;
+                break;
+            default:
+                gradeText.text = "";
+                break;
+        }
+    
     }
 
     public void GoalRestart()
